a1c59a366333e10819f2b9ded2092cedc3323e3c
yaroslavyakovkin <yaroslav.y2010@ya.ru>
2024-01-22 07:56:55 +0300

    last changes from notebook

--- core/handlers/balance.py
+++ core/handlers/balance.py
@@ -1,5 +1,5 @@
 from aiogram import Bot
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from core.data.sql import get_all_users
 
@@ -9,7 +9,7 @@ async def listofusers(message:Message, bot:Bot):
     users = await get_all_users()
     for user in users:
         user_id, username, balance = user
-        list_users.button(text=f"@{username} - {balance}₽", callback_data=f"{user_id}")
+        list_users.button(text=f"@{username} - {balance}₽", callback_data=f"id:balance:{user_id}")
     list_users.adjust(1)
 
     await message.answer('Список пользователей:', reply_markup=list_users.as_markup())
--- core/handlers/callback.py
+++ core/handlers/callback.py
@@ -5,12 +5,13 @@ from core.keyboards.inline import pay_inline
 
 
 async def backcall(call:CallbackQuery, bot:Bot, message:Message):
-    if call.data == 'cash':
-        await call.message.edit_media(InputMediaPhoto(media = FSInputFile('core\\data\\file\\resourses\\qr-code.jpg')),
-                                    reply_markup=pay_inline)
 
     for user in await get_all_users():
         user_id, username, balance = user
-        if call.data == f'{user_id}':
+        if call.data == f'id:balance:{user_id}':
             text = f'Выбран пользователь @{username}, его баланс {balance}₽\n Что будем делать?'
-            await call.message.answer(text, reply_markup=None)
+            await bot.message.answer(text, reply_markup=None)
+
+    if call.data == 'cash':
+        await call.message.edit_media(InputMediaPhoto(media = FSInputFile('core\\data\\file\\resourses\\qr-code.jpg')),
+                                    reply_markup=pay_inline)
