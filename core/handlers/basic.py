a2262e4d27368db96c687d34403a9efa7a7e9f45
yaroslavyakovkin <yaroslav.y2010@ya.ru>
2024-01-22 22:48:01 +0300

    admin keyboard

--- core/handlers/basic.py
+++ core/handlers/basic.py
@@ -1,7 +1,8 @@
 from aiogram import Bot
 from aiogram.types import Message
 from core.data.sql import create_user, db_start
-from core.util.commands import adminoruser, commands
+from core.util.commands import commands, is_admin
+from core.keyboards.start import main_keyboard, admin_keyboard
 
 
 async def start_bot(bot: Bot):
@@ -13,10 +14,14 @@ async def stop_bot(bot: Bot):
     print('Stop')
 
 async def get_start(message: Message, bot = Bot):
-        await create_user(user_id=message.from_user.id, username=message.from_user.username)
-        await message.answer(f'''Здравствуйте, {message.from_user.first_name}.
+    if is_admin(message.from_user.id) != True: keyboard = main_keyboard
+    else: keyboard = admin_keyboard
+    await create_user(user_id=message.from_user.id, username=message.from_user.username)
+    await message.answer(f'''Здравствуйте, {message.from_user.first_name}.
 Добро пожаловать в систему автоматизированной печати!
-Для продолжения, нажмите на одну из интересующих Вас кнопок.''', reply_markup=adminoruser(message.from_user.id))
+Для продолжения, нажмите на интересующую кнопоку''', reply_markup=keyboard)
 
 async def echo(message:Message):
-    await message.reply('idk bro', reply_markup=adminoruser(message.from_user.id))
+    if is_admin(message.from_user.id) != True: keyboard = main_keyboard
+    else: keyboard = admin_keyboard
+    await message.reply('Я не знаю такой команды 🥺', reply_markup=keyboard)
