ba71df841a5462eae9c74ff00ce10365326f007c
yaroslavyakovkin <yaroslav.y2010@ya.ru>
2024-01-22 22:48:46 +0300

    loging, register new commands

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
-import logging, asyncio, os
+import logging, asyncio, os, datetime
 from dotenv import load_dotenv as ld
 from aiogram import Bot, Dispatcher, F
-from aiogram.filters import Command, CommandObject
+from aiogram.filters import Command
 from core.handlers.basic import get_start, echo, start_bot, stop_bot
 from core.handlers.pay import wannapay
-from core.handlers.balance import listofusers
+from core.handlers.balance import listofusers, edit_balance
 from core.handlers.callback import backcall
 
 
@@ -13,6 +13,7 @@ TOKEN = os.getenv('API_TOKEN')
 
 
 logging.basicConfig(level=logging.INFO, 
+                    filename=f'core\\data\\files\\logs\\printbot_{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log',
                     format='%(asctime)s - [%(levelname)s] - %(name)s - %(message)s')
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
@@ -20,9 +21,10 @@ dp = Dispatcher()
 async def main():
     dp.startup.register(start_bot)
     dp.shutdown.register(stop_bot)
-    dp.message.register(get_start, Command('start'))
+    dp.message.register(get_start, Command('start'), F.text.lower == 'старт')
+    dp.message.register(edit_balance, Command('b'))
     dp.message.register(wannapay, F.text == '💳 Баланс')
-    dp.message.register(listofusers, F.text == '💳 Баланс пользователей', )
+    dp.message.register(listofusers, F.text == '💳 Баланс пользователей')
     dp.callback_query.register(backcall)
     dp.message.register(echo)
     try:
