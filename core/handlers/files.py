20d98fc50e2f002d35e8b096074e627784722846
yaroslavyakovkin <yaroslav.y2010@ya.ru>
2024-01-26 04:22:11 +0300

    size limit, if page = 0, tips

--- core/handlers/files.py
+++ core/handlers/files.py
@@ -19,67 +19,77 @@ f'Для создания заявки на печать, просто отпр�
 parse_mode=ParseMode.HTML)
 
 async def sendfile(message:Message, bot:Bot):
+    if message.document.file_size <= 10485760:
+        name, ext = os.path.splitext(message.document.file_name)
+        if ext in ALLOWED_EXTENSIONS:
+            if await get_fileinfo(message.document.file_unique_id) != None:
+                await message.reply(f'<i>Файл <code>{name}</code> уже находится в очереди на печать!</i>',
+                                    parse_mode=ParseMode.HTML)
+            else:
+                path = f'core\\data\\files\\temp\\{message.from_user.id}'
+                if not os.path.exists(path):os.makedirs(path)
+                pdf = f'{path}\\{name}.pdf'
+                tempath = f'{path}\\{message.document.file_name}'
+                file_id = message.document.file_id
+                file_info = await bot.get_file(file_id)
+                file_path = file_info.file_path
+                downloaded_file = await bot.download_file(file_path)
+                with open(tempath, "wb") as new_file:
+                    new_file.write(downloaded_file.read())
+                    
+                msg = await bot.send_document(message.chat.id, file_id, 
+                                    caption=f'<i>Файл <code>{name}</code> принят!</i>',
+                                    parse_mode=ParseMode.HTML)
+                await asyncio.sleep(2)
+                if ext != '.pdf':
+                    await bot.edit_message_caption(message.chat.id, msg.message_id,
+                                        caption=f'<i>Файл: <code>{name}</code> принят!\
+                                        \n\n<b>Произвожу конвертацию {ext} --> .pdf</b></i>',
+                                        parse_mode=ParseMode.HTML)
 
-    name, ext = os.path.splitext(message.document.file_name)
-    
-    if ext in ALLOWED_EXTENSIONS:
-        if await get_fileinfo(message.document.file_unique_id) != None:
-            await message.reply(f'<i>Файл <code>{name}</code> уже находится в заявке!</i>',
-                                parse_mode=ParseMode.HTML)
-        else:
-            path = f'core\\data\\files\\temp\\{message.from_user.id}'
-            if not os.path.exists(path):os.makedirs(path)
-            pdf = f'{path}\\{name}.pdf'
-            tempath = f'{path}\\{message.document.file_name}'
-            file_id = message.document.file_id
-            file_info = await bot.get_file(file_id)
-            file_path = file_info.file_path
-            downloaded_file = await bot.download_file(file_path)
-            with open(tempath, "wb") as new_file:
-                new_file.write(downloaded_file.read())
-                
-            msg = await bot.send_document(message.chat.id, file_id, 
-                                caption=f'<i>Файл <code>{name}</code> принят!</i>',
-                                parse_mode=ParseMode.HTML)
-            await asyncio.sleep(2)
-            if ext != '.pdf':
+                    d2p(tempath)
+                    os.remove(tempath)
+                        
+                    msg = await bot.edit_message_media(InputMediaDocument(media=FSInputFile(pdf),
+                                        caption=f'<i>Файл: <code>{name}</code> принят!\
+                                        \n\n<b>Произвожу оценку стоимости...</b></i>',
+                                        parse_mode=ParseMode.HTML),
+                                        message.chat.id, msg.message_id)
+                                        
                 await bot.edit_message_caption(message.chat.id, msg.message_id,
                                     caption=f'<i>Файл: <code>{name}</code> принят!\
-                                    \n\n<b>Произвожу конвертацию {ext} --> .pdf</b></i>',
-                                    parse_mode=ParseMode.HTML)
-
-                d2p(tempath, pdf)
-                os.remove(tempath)
-                    
-                msg = await bot.edit_message_media(InputMediaDocument(media=FSInputFile(pdf),
-                                    caption=f'<i>Файл: <code>{name}</code> принят!\
                                     \n\n<b>Произвожу оценку стоимости...</b></i>',
-                                    parse_mode=ParseMode.HTML),
-                                    message.chat.id, msg.message_id)
-                                    
-            await bot.edit_message_caption(message.chat.id, msg.message_id,
-                                caption=f'<i>Файл: <code>{name}</code> принят!\
-                                \n\n<b>Произвожу оценку стоимости...</b></i>',
-                                parse_mode=ParseMode.HTML)
-            
-            pages, fill = await price_alg(pdf,path)
-            price = f'{(pages+fill*50):.2f}'
+                                    parse_mode=ParseMode.HTML)
+                
+                pages, fill = await price_alg(pdf)
+                if pages == 0:
+                    await bot.delete_message(message.chat.id, msg.message_id)
+                    await bot.send_message(message.chat.id,'🤡', protect_content=True)
+                else:
+                    price = f'{(pages+fill*50):.2f}'
 
-            newfile = InlineKeyboardBuilder()
-            newfile.button(text='✗ Отменить', callback_data=f'newfile:cancel')
-            newfile.button(text='✓ Продолжить', callback_data=f'newfile:accept:{price}')
-            newfile.adjust(2)
+                    newfile = InlineKeyboardBuilder()
+                    newfile.button(text='✗ Отменить', callback_data=f'newfile:cancel')
+                    newfile.button(text='✓ Продолжить', callback_data=f'newfile:accept:{price}')
+                    newfile.adjust(2)
 
 
-            await bot.edit_message_caption(message.chat.id, msg.message_id,
-                                caption=f'<i>Файл: <code>{name}</code>{ext}\n\
-                                \nКоличество листов: <code>{pages}</code>\
-                                \nСредняя заполненность: <code>{int(fill*100/pages)}%</code>\
-                                \nCтоимость печати: <code>{price}</code></i> ₽',
-                                parse_mode=ParseMode.HTML, reply_markup=newfile.as_markup())
-            os.remove(pdf)
+                    await bot.edit_message_caption(message.chat.id, msg.message_id,
+                                        caption=f'<i>Файл: <code>{name}</code>{ext}\n\
+                                        \nКоличество листов: <code>{pages}</code>\
+                                        \nСредняя заполненность: <code>{int(fill*100/pages)}%</code>\
+                                        \nCтоимость печати: <code>{price}</code></i> ₽',
+                                        parse_mode=ParseMode.HTML, reply_markup=newfile.as_markup())
+                os.remove(pdf)
+        else:
+            await message.reply(
+                f'<i><b>Ой, файл не того формата!</b>\
+                \n\nЯ принимаю только:</i>\
+                \n<b>Файлы менее</b> <code>10</code> мб\
+                \n<code>.pdf .doc .docx</code>', parse_mode=ParseMode.HTML)
     else:
         await message.reply(
-            f'<i><b>Ой, попробуйте другой файл!</b>\
-            \n\nЯ принимаю только:</i>\
-            \n<code>.pdf .doc .docx</code>', parse_mode=ParseMode.HTML)
+                f'<i><b>Ой, файл слишком большой!</b>\
+                \n\nЯ принимаю только:</i>\
+                \n<b>Файлы менее</b> <code>10</code> мб\
+                \n<code>.pdf .doc .docx</code>', parse_mode=ParseMode.HTML)
